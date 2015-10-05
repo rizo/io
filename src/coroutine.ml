bebf50797afa3f2b0918d92c9cfeb7650c999036
Rizo Isrof <rizo@odis.io>
2015-10-05 16:50:48 +0100

    Added iteratee implementation.

--- src/coroutine.ml
+++ src/coroutine.ml
@@ -79,31 +79,56 @@ end = struct
       | Cont (a, k) -> run' Fn.(f @. (cons a)) k
       | Done x -> (f [], x)
     in run' id g
-
-  (* let rec zip_with f t1 t2 = fun () -> *)
-    (* match t1 (), t2 () with *)
-    (* | Cont a, Cont b -> Cont (zip_with f a b) *)
-    (* | Cont a, Done b -> Cont (zip_with f a (pure b)) *)
-    (* | Done a, Cont b -> Cont (zip_with f (pure a) b) *)
-    (* | Done a, Done b -> Done (f a b) *)
-
-  (* let interleave xs = *)
-    (* List.fold_right (zip_with cons) xs (pure []) *)
 end
 
 
+module Iteratee : sig
+  type ('a, 'x) iter = unit -> ('a, 'x) status'
+   and ('a, 'x) status' =
+    | Cont of ('a -> ('a, 'x) iter)
+    | Done of 'x
+  val pure : 'x -> ('a, 'x) iter
+  val ( >>= ) : ('a, 'x) iter -> ('x -> ('a, 'y) iter) -> ('a, 'y) iter
+  val ( >> ) : ('a, unit) iter -> ('a, 'x) iter -> ('a, 'x) iter
+  val await : ('a, 'a) iter
+  val run : 'a list -> ('a, 'x) iter -> 'x
+end = struct
+  type ('a, 'x) iter = unit -> ('a, 'x) status'
+   and ('a, 'x) status' =
+    | Cont of ('a -> ('a, 'x) iter)
+    | Done of 'x
+
+  let pure x = fun () -> Done x
+
+  let rec (>>=) g f = fun () ->
+    match g () with
+    | Cont k -> Cont (fun a -> k a >>= f)
+    | Done x -> (f x) ()
+
+  let (>>) g1 g2 =
+    g1 >>= fun () -> g2
+
+  let await = fun () -> Cont pure
+
+  let rec run xs i =
+    match xs, i () with
+    | a::rest, Cont k -> run rest (k a)
+    | a::rest, Done x -> x
+    | []     , Cont k -> fail "no more values to feed."
+    | []     , Done x -> x
+end
 
 module Test_trampoline = struct
   open Trampoline
 
-  let hello name = begin
+  let trampoline = begin
     (fun () -> Done (print_string "hello, ")) >>
     pause >>
-    (fun () -> Done (print name))
+    (fun () -> Done (print "world"))
   end
 
-  let test_hello () =
-    match (hello "world") () with
+  let test () =
+    match trampoline () with
     | Cont k -> print_string "wonderful "; run k
     | Done x -> fail "could not bounce"
 end
@@ -119,4 +144,22 @@ module Test_generator = struct
     (fun () -> Done (print_string "returning three: ")) >>
     pure 3
   end
+
+  let test () =
+    run gen
 end
+
+module Test_iteratee = struct
+  open Iteratee
+
+  let iter = begin
+    (fun () -> Done (print_string "enter two numbers: ")) >>
+    await >>= fun a ->
+    await >>= fun b ->
+    (fun () -> Done (print_endline ("sum is " ^ string_of_int (a + b))))
+  end
+
+  let test () =
+    run [2; 5] iter
+end
+

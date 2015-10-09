55cb5cc5f4a086cf0a7300448e4cb765b3148ec3
Rizo Isrof <rizo@odis.io>
2015-10-09 15:55:59 +0100

    Updated tests.

--- test/test_flow.ml
+++ test/test_flow.ml
@@ -4,14 +4,17 @@ open Flow
 
 
 let test_monad () = begin
+  print_endline "test_monad";
   assert (((fun () -> Ready ()) >>= return) () = Ready ());
   assert (run (yield 1 =>= (await >>= yield) => nth 0) = 1);
 end
 
 let test_api () = begin
+  print_endline "test_api";
   assert (run (of_list (List.range 10 100) =>= take 10 =>= tail => len) = 9);
-  assert (run (of_list (List.iota 1000000) =>= take 10 =>= filter even => sum) = 20);
+  assert (run (of_list (List.range 10 100) =>= take 10 =>= filter even => sum) = 70);
   assert (run (of_list (List.range 50 100) =>= take 10 => fold (+) 0) = 545);
+  assert (run (iota 100000000000 =>= take 10 =>= filter even => sum) = 20);
   assert (run (iota 1000000 =>= filter odd => fold (+) 0) = 250000000000);
   assert (run (yield 1 >> yield 2 => collect) = [1; 2]);
 end
